501dc541786cc7fe95150653ed473faf78530bb8
zhaoYangguang <1163765691@qq.com>
2020-11-23 18:13:14 +0800

    fix invite bot timeout in faderation

--- matrix.go
+++ matrix.go
@@ -58,11 +58,17 @@ func (mx *MatrixHandler) HandleEncryption(evt *event.Event) {
 func (mx *MatrixHandler) joinAndCheckMembers(evt *event.Event, intent *appservice.IntentAPI) *mautrix.RespJoinedMembers {
 	resp, err := intent.JoinRoomByID(evt.RoomID)
 	if err != nil {
-		time.Sleep(3 * time.Second)
+		mx.log.Debugfln("JoinRoomByID err, retry in 5 seconds", err)
+		time.Sleep(5 * time.Second)
 		resp, err = intent.JoinRoomByID(evt.RoomID)
 		if err != nil {
-			mx.log.Debugfln("Failed to join room %s as %s with invite from %s: %v", evt.RoomID, intent.UserID, evt.Sender, err)
-			return nil
+			mx.log.Debugfln("JoinRoomByID err, retry again in 5 seconds", err)
+			time.Sleep(5 * time.Second)
+			resp, err = intent.JoinRoomByID(evt.RoomID)
+			if err != nil {
+				mx.log.Debugfln("Failed to join room %s as %s with invite from %s: %v", evt.RoomID, intent.UserID, evt.Sender, err)
+				return nil
+			}
 		}
 	}
 
