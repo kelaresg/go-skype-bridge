f892087014b1e635afa472730aab9f65a5f66d00
Dan Pastusek <dan@hexial.com>
2020-12-21 16:02:22 -0700

    fix for postgres schemas

--- database/upgrades/2020-08-25-message-id-column.go
+++ database/upgrades/2020-08-25-message-id-column.go
@@ -6,7 +6,7 @@ import (
 
 func init() {
 	upgrades[17] = upgrade{"Add id column to messages", func(tx *sql.Tx, ctx context) error {
-		_, err := tx.Exec(`ALTER TABLE message ADD COLUMN id CHAR(13) DEFAULT ""`)
+		_, err := tx.Exec(`ALTER TABLE message ADD COLUMN id CHAR(13) DEFAULT ''`)
 		if err != nil {
 			return err
 		}
