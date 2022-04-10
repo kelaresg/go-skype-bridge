5f9ece8e0a7f3a053aa15f707c1169eb2f5d4ccf
Otto Hollmann <otto@hollmann.cz>
2022-04-10 11:43:12 +0200

    Rename database upgrade.

--- database/upgrades/2022-04-02-add-password.go
+++ database/upgrades/2022-04-02-add-password.go
@@ -5,7 +5,7 @@ import (
 )
 
 func init() {
-	upgrades[20] = upgrade{"Add password columns to user table.", func(tx *sql.Tx, c context) error {
+	upgrades[20] = upgrade{"Add password column to user table.", func(tx *sql.Tx, c context) error {
 		if c.dialect == Postgres {
 			_, err := tx.Exec(`ALTER TABLE "user" ADD COLUMN password VARCHAR(255)`)
 			if err != nil {
